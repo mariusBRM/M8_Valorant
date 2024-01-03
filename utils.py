c3b8beacaa0c37b6e684fff0fb23e94886e9692d
mariusBRM <marius.reymauzaize@gmail.com>
2024-01-03 18:30:46 +0000

    [UPDATE] modify utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,52 @@
+import pandas as pd
 
 # Set general constant
 SIDE = {'Total' : 0, 'Side 1' : 1, 'Side 2' : 2}
 NAME = ['logaN', 'Wailers', 'beyAz', 'TakaS', 'nataNk']
-
-class BasicStatistique:
+TEAM = "Gentle Mates"
+class BasicStatistics:
 
     #TODO: Change the return type of number_action_avg_by_player_by_scope in the case there are mutliple Names
     # should be able to get all of the change
+    def calculate_action_player(self, name, data, action, side):
+        report = {name : 0}
+        try:
+            side_ = SIDE[side]
+        except NameError:
+            raise NameError(f'No side named {side}')
+        
+        if action not in ['K', 'D']:
+            action_ = 'K'
+        else:
+            action_ = action
+        
+        #TODO: continue
+        return 0
+    
+    def calculate_action_team(self, names, data, action, side):
+        report = {n:0 for n in names}
+
+
+        return 0
+    
+    def filter_by_scope(self, data, scope):
+
+        if scope:
+            # the scope is specified
+            try:
+                data = data[data["Series"] == scope]
+                return data
+            except NameError:
+                print(f'side {scope} is not specified so return the all data.')
+                return data
+        else:
+            # by default the all dataset
+            return data
+        
     def average(list):
         return sum(list) / len(list)
 
-    def number_action_avg_by_player_by_scope(self, df, scope, action, side, name = NAME):
+    def number_action_avg_by_player_by_scope(self, df, action, side = None, scope = None, name = None, team_name = None):
         """
             Function that calculates the average kill/death of each player by scope
 
@@ -22,7 +58,24 @@ class BasicStatistique:
         """
         try:
             processed_kills = []
-            kills = df[df['Player Name'].isin(name)]
+            if name:
+                # want statistic on a particular player
+                data_player = df[df['Player Name'] == name]
+                data_player_filtered_by_scope = self.filter_by_scope(data_player, scope)
+                report_player = self.calculate_action_player(data_player_filtered_by_scope, action, side)
+
+            elif team_name:
+                # want statistic on the all team
+                set_names = set(df['Player Name'].where(df['Team Name'] == team_name))
+                names = pd.DataFrame(list(set_names), columns=['Player']).dropna()['Player'].values.tolist()
+                data_team = df[df['Player Name'].isin(names)]
+                data_team_filtered_by_scope = self.filter_by_scope(data_team, scope)
+                report_team = self.calculate_action_team(data_team_filtered_by_scope, action, side)
+            else:
+                raise NameError('No instance can be treated')
+            
+            
+
             if scope != "Tournament" and scope in set(kills['Series']):
                 # average in a particular Match
                 kills = kills[kills['Series'] == scope]
@@ -39,7 +92,7 @@ class BasicStatistique:
                 
         except NameError:
             print(f'Cannot fetch kills because {NameError}')
-    
+
     def number_action_by_player_by_scope(df, scope, action, side, name = NAME):
         """Function that calculates the kills/deaths of each player by scope
 
