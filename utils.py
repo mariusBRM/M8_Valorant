da677ada44f940b432644d2b292950213ca42b8e
mariusBRM <marius.reymauzaize@gmail.com>
2024-01-07 21:23:25 +0000

    [UPDATE] Add score combat and damage score

--- utils.py
+++ utils.py
@@ -4,120 +4,196 @@ import pandas as pd
 SIDE = {'Total' : 0, 'Side 1' : 1, 'Side 2' : 2}
 NAME = ['logaN', 'Wailers', 'beyAz', 'TakaS', 'nataNk']
 TEAM = "Gentle Mates"
-class BasicStatistics:
 
-    #TODO: Change the return type of number_action_avg_by_player_by_scope in the case there are mutliple Names
-    # should be able to get all of the change
-    def __init__(self):
-        self.side = {'Total' : 0, 'Side 1' : 1, 'Side 2' : 2}
+def calculate_action_player(name, data, average, action, side):
+    """
+    Functions that calculate the number of action by side
 
-    def calculate_action_player(self, name, data, average, action, side):
-        """ 
-        Function that calculate the average action  (kills/death) of a player for a given side (attack, defense or total) 
-        returns the name and the average kill of the player
-        """
-        report = {name : 0}
-        actions = []
-
-        try:
-            side_ = self.side[side]
-        except NameError:
-            raise NameError(f'No side named {side}')
-        
-        if action not in ['K', 'D']:
-            action_ = 'K'
-        else:
-            action_ = action
-        
-        for i,value in enumerate(data):
-            actions.append(int(data[action_].iloc[i].split('\n')[side_]))
-        
-        if average:
-            report[name] = self.average(actions)
-        else:
-            report[name] = sum(actions)
-        return report
+    parameter:
+        name : name of the player | string
+        data : dataframe of the retrieve data from the scraper
+        average : boolean if true then calculate the average, sum otherwise
+        action : either 'K' or 'D'
+        side : ATK, DFS or full match
     
-    def calculate_action_team(self, names, data, average, action, side):
-        """ 
-        Function that calculate the average action  (kills/death) of a player for a given side (attack, defense or total) 
-        returns the name and the average kill of each player of the team
-        """
-        report = {n:[] for n in names}
-        
-        try:
-            side_ = self.side[side]
-        except NameError:
-            raise NameError(f'No side named {side}')
-        
-        if action not in ['K', 'D']:
-            action_ = 'K'
-        else:
-            action_ = action
+    return:
+        report : a dictionnary with the name as key and the calculated value
+    """
+    report = {name: 0}
+    actions = []
 
-        for i,value in enumerate(data):
-            name = value['Player Name']
-            report[name].append(int(data[action_].iloc[i].split('\n')[side_]))
-        
-        if average:
-            final_report = {n:self.average(report[n]) for n in names}
-        else:
-            final_report = {n:sum(report[n]) for n in names}
-        return final_report
+    side_mapping = {'Total': 0, 'Side 1': 1, 'Side 2': 2}
+    side_ = side_mapping.get(side, None)
     
-    def filter_by_scope(self, data, scope):
+    if side_ is None:
+        side_ = 0
+    
+    action_ = action if action in ['K', 'D'] else 'K'
+    
+    for i, _ in enumerate(data):
+        actions.append(int(data[action_].iloc[i].split('\n')[side_]))
+    
+    if average:
+        report[name] = calculate_average(actions)
+    else:
+        report[name] = sum(actions)
+    
+    return report
 
-        if scope:
-            # the scope is specified
-            try:
-                data = data[data["Series"] == scope]
-                return data
-            except NameError:
-                print(f'side {scope} is not specified so return the all data.')
-                return data
-        else:
-            # by default the all dataset
+def calculate_action_team(names, data, average, action, side):
+    """
+    Functions that calculate the number of action by side by player for the all team
+
+    parameter:
+        names : names of the player of the team | list of string
+        data : dataframe of the retrieve data from the scraper
+        average : boolean if true then calculate the average, sum otherwise
+        action : either 'K' or 'D'
+        side : ATK, DFS or full match
+    
+    return:
+        report : a dictionnary with the names as keys and the calculated value for each name
+    """
+    report = {n: [] for n in names}
+    
+    side_mapping = {'Total': 0, 'Side 1': 1, 'Side 2': 2}
+    side_ = side_mapping.get(side, None)
+    
+    if side_ is None:
+        side_ = 0
+    
+    action_ = action if action in ['K', 'D'] else 'K'
+    
+    for i, _ in enumerate(data):
+        name = data.iloc[i]['Player Name']
+        report[name].append(int(data[action_].iloc[i].split('\n')[side_]))
+    
+    if average:
+        final_report = {n: calculate_average(report[n]) for n in names}
+    else:
+        final_report = {n: sum(report[n]) for n in names}
+    
+    return final_report
+
+def calculate_score_player(data,name, type, side):
+
+    report = {name: 0}
+    scores = []
+
+    side_mapping = {'Total': 0, 'Side 1': 1, 'Side 2': 2}
+    side_ = side_mapping.get(side, None)
+    
+    if side_ is None:
+        side_ = 0
+    
+    for i,_ in enumerate(data):
+        scores.append(int(data[type].iloc[i].split('\n')[side_]))
+    return 0
+
+def calculate_score_team():
+    return 0
+
+def filter_by_scope(data, scope):
+    """
+    Function that keep only the wanted match, get the all tournament by default
+
+    parameter:
+        data : dataframe of the retrieve data from the scraper
+        scope : string representing the instance that is under study i.e the match or the all tournament
+    """
+    if scope:
+        try:
+            data = data[data["Series"] == scope]
             return data
-        
-    def average(list):
-        return sum(list) / len(list)
+        except KeyError:
+            print(f'Side {scope} is not specified so returning all data.')
+            return data
+    else:
+        return data
 
-    def number_action_by_player_by_scope(self, df, action, average,side = None, scope = None, name = None, team_name = None):
-        """
-            Function that calculates the average kill/death of each player by scope
+def calculate_average(lst):
+    return sum(lst) / len(lst)
 
-            parameter:
-                df : data set
-                scope : string, 'Tournament' for the all, values in the column Series for discretized
-                action : string 'K', 'D'
-                side : string 'Total', 'Side 1', 'Side 2'
-        """
+def number_action_by_player_by_scope(df, action, average, side=None, scope=None, name=None, team_name=None):
+    """
+    Functions that calculate the number of action by side by player and by scope
 
-        if name is not None:
-            # want statistic on a particular player
-            try:
-                data_player = df[df['Player Name'] == name]
-            except NameError:
-                print(f'No such {name} in the dataset : {NameError}')
-            data_player_filtered_by_scope = self.filter_by_scope(data_player, scope)
-            report_player = self.calculate_action_player(name, data_player_filtered_by_scope,average,action, side)
+    parameter:
+        df : dataframe of the retrieve data from the scraper
+        average : boolean if true then calculate the average, sum otherwise
+        scope : string representing the instance that is under study i.e the match or the all tournament set to None by default
+        name : name of the player | string
+        action : either 'K' or 'D'
+        side : ATK, DFS or full match set to None by default
+        team_name : name of the teamp that is under study | string
+    
+    return:
+        report : a dictionnary with the names as keys and the calculated value for each name
+    """
+    if name is not None:
+        try:
+            data_player = df[df['Player Name'] == name]
+        except NameError:
+            print(f'No such {name} in the dataset : {NameError}')
+        data_player_filtered_by_scope = filter_by_scope(data_player, scope)
+        report_player = calculate_action_player(name, data_player_filtered_by_scope, average, action, side)
 
-            return report_player
+        return report_player
 
-        elif team_name is not None:
-            # want statistic on the all team
-            try:
-                set_names = set(df['Player Name'].where(df['Team Name'] == team_name))
-            except NameError:
-                print(f'No such {set_names} in the dataset : {NameError}')
+    elif team_name is not None:
+        try:
+            set_names = set(df['Player Name'].where(df['Team Name'] == team_name))
+        except NameError:
+            print(f'No such {set_names} in the dataset : {NameError}')
 
-            names = pd.DataFrame(list(set_names), columns=['Player']).dropna()['Player'].values.tolist()
-            data_team = df[df['Player Name'].isin(names)]
-            data_team_filtered_by_scope = self.filter_by_scope(data_team, scope)
-            report_team = self.calculate_action_team(names,data_team_filtered_by_scope, average,action, side)
+        names = pd.DataFrame(list(set_names), columns=['Player']).dropna()['Player'].values.tolist()
+        data_team = df[df['Player Name'].isin(names)]
+        data_team_filtered_by_scope = filter_by_scope(data_team, scope)
+        report_team = calculate_action_team(names, data_team_filtered_by_scope, average, action, side)
 
-            return report_team
-        else:
-            raise NameError('No instance can be treated')
+        return report_team
+    else:
+        raise ValueError('No instance can be treated')
+    
+def average_score_by_player_by_scope(df, type, side=None, scope=None, name=None, team_name=None):
+    """
+    Functions that calculate the number of action by side by player and by scope
+
+    parameter:
+        df : dataframe of the retrieve data from the scraper
+        type : type of the score wanted | 'ACS' or 'ADR'
+        scope : string representing the instance that is under study i.e the match or the all tournament set to None by default
+        name : name of the player | string
+        action : either 'K' or 'D'
+        side : ATK, DFS or full match set to None by default
+        team_name : name of the teamp that is under study | string
+    
+    return:
+        report : a dictionnary with the names as keys and the calculated value for each name
+    """
+    if name is not None:
+        try:
+            data_player = df[df['Player Name'] == name]
+        except NameError:
+            print(f'No such {name} in the dataset : {NameError}')
+        data_player_filtered_by_scope = filter_by_scope(data_player, scope)
+        report_player = calculate_score_player(name, data_player_filtered_by_scope, type, side)
+
+        return report_player
+
+    elif team_name is not None:
+        try:
+            set_names = set(df['Player Name'].where(df['Team Name'] == team_name))
+        except NameError:
+            print(f'No such {set_names} in the dataset : {NameError}')
+
+        names = pd.DataFrame(list(set_names), columns=['Player']).dropna()['Player'].values.tolist()
+        data_team = df[df['Player Name'].isin(names)]
+        data_team_filtered_by_scope = filter_by_scope(data_team, scope)
+        report_team = calculate_score_team(names, data_team_filtered_by_scope, type, side)
+
+        return report_team
+    else:
+        raise ValueError('No instance can be treated')
     
 
